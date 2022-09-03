2addd5dce2cd713dec84f60fc4b4e340a349d6d7
Leonardo Miranda <leonardo.miranda@convenia.com.com.br>
2022-09-03 14:55:18 -0300

    First commit - project setup and simple rating system done without using store yet

---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let count;
+  export let average;
+
+  $: review = count != 1 ? "Reviews" : "Review";
+  $: avg = average ? average : 0;
+</script>
+
+<div class="feedback-stats">
+  <h4>
+    {count}
+    {review}
+  </h4>
+  <h4>Ratings Average: {avg}</h4>
+</div>
+
+<style>
+  .feedback-stats {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
